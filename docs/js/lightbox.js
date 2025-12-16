6344d322e1dc7ab855ff99dd7a86821ce06acd85
xacobe <xacobe.net@gmail.com>
2025-12-16 20:00:56 +0100

    remove caption

--- docs/js/lightbox.js
+++ docs/js/lightbox.js
@@ -4,7 +4,7 @@ function initLightbox() {
   if (!lightboxInitialized) {
     baguetteBox.run('.gallery', {
       buttons: false,
-      captions: true,
+      captions: false,
       bodyClass: "lightbox--open",
       async: true,
       closeX: '<svg width="20" height="20"><g stroke="rgb(160,160,160)" stroke-width="4"><line x1="5" y1="5" x2="20" y2="20"></line><line x1="5" y1="20" x2="20" y2="5"></line></g></svg>'
